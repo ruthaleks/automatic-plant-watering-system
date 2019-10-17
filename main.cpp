a3653e609d45838f257d89fc667f49ad17055fb3
ruthaleks <ruthaleks@gmail.com>
2019-10-17 13:21:19 +0200

    changed sensor

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int32_t main()
     std::cout << "** Start of main **\n";
     Tank tank{ SensorType::SIM_LoadSensor, ActuatorType::SIM_Switch }; 
     std::cout << "Created a new water tank\n";    
-    Pot pot{ SensorType::SIM_MoistSensor };
+    Pot pot{ SensorType::I2C_Sensor };
     std::cout << "Created a new pot \n";
 
 

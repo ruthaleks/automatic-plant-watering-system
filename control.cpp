69ec28d8021c4790a7b2f46ed53b46405ab0fbed
ruthaleks <ruth.aleks@gmail.com>
2019-10-23 17:40:11 +0200

    new funtion write_to_file, currently not used

--- control.cpp
+++ control.cpp
@@ -1,14 +1,19 @@
 #include <chrono>
 #include <cstdint>
-#include <memory>
+#include <cstring>
+#include <fstream>
+#include <ctime>
 #include <iostream>
+#include <memory>
 #include <thread>
 
-#include "devices.hpp"
-#include "sensorMonitor.hpp"
+#include "lib/expected.h"
+
 #include "actuatorController.hpp"
+#include "devices.hpp"
 #include "parameters.hpp"
 #include "potManager.hpp"
+#include "sensorMonitor.hpp"
 #include "tankManager.hpp"
 
 
@@ -24,7 +29,8 @@ int32_t control_routine(PotManager& pot, TankManager& tank)
     #ifdef DEBUG
         std::cout << "Check if the soil is dry \n"; 
     #endif
-        if (pot.is_dry()){
+        util::Expected<int32_t> current_humidity = pot.humidity();
+        if (pot.is_dry( current_humidity )){
             #ifdef DEBUG
             std::cout << "Soil is dry, need to add water \n";
             #endif
@@ -34,9 +40,27 @@ int32_t control_routine(PotManager& pot, TankManager& tank)
         #ifdef DEBUG
         std::cout << "Wait for " << t << "s\n";
         #endif
-        std::this_thread::sleep_for(std::chrono::seconds( t ));
+        err = 1;
+        //std::this_thread::sleep_for(std::chrono::seconds( t ));
     }
     return 0;
+}
+
+void write_to_file( float data ) 
+{
+    #ifdef DEBUG
+    std::cout << "Writing data to file\n";
+    #endif
+    // Get the time stamp in the correct format
+    auto time =  std::chrono::system_clock::to_time_t( 
+        std::chrono::system_clock::now());
+    auto time_stamp = std::ctime( &time );
+    char *t = std::strtok(time_stamp, "\n");
+    
+    std::ofstream file;
+    file.open( "data.txt", std::fstream::app );
+    file << t << " Humidity: " << data << '\n';
+    file.close();
 }
 
 

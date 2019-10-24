af877d0d0b2d7d32347ef0ae7d48e7feb8ef8cad
ruthaleks <ruth.aleks@gmail.com>
2019-10-24 17:59:52 +0200

    added extra security check with allowed sensor values

--- potManager.cpp
+++ potManager.cpp
@@ -18,27 +18,34 @@
 PotManager::PotManager( SensorType sensor_type) :
  m_sensor_ptr{ new SensorMonitor{sensor_type} }{}
 
-bool PotManager::is_dry( util::Expected<int32_t> hum ) const
+bool PotManager::is_dry( uint32_t hum ) const
 // checks the sensor reading if the soil is dry and return true, else false 
 {  
-    if ( hum.isValid() ){
-        if ( (uint32_t) hum.get() < m_threashold ) {
-            return true;  
-        }
+    if (!m_minmax_is_set ) {
+         print::error_msg("Allowed sensor value range is not set.\n");
+         return false;
     }
-    return false; // soil is dry or sensor error 
+    if( !m_threashold ) {
+        print::error_msg("A sensor threashold value is not set.\n");
+        return false;
+    }
+   
+    if ( (hum >= m_min) && (hum <= m_max) && (hum < m_threashold) ) {
+        return true;  
+    }
+    return false; // soil is dry or error 
 }
 
-util::Expected<int32_t> PotManager::humidity() const 
+util::Expected<uint32_t> PotManager::humidity() const 
 // The sensor readings from a specified time period (m_sample_period * m_samples) are collected
 // and an average value is calculated  
 {   
-    std::vector<int32_t> sensor_data{};
+    std::vector<uint32_t> sensor_data{};
     #ifdef DEBUG
     std::cout << "Read data from humidity sensor..\n";  
     #endif 
     for (uint32_t n = 0; n < m_sampling_time; n++){
-        util::Expected<int32_t> value = m_sensor_ptr->value();
+        util::Expected<uint32_t> value = m_sensor_ptr->value();
         if (value.isValid()){
             sensor_data.push_back( value.get() );
             std::this_thread::sleep_for(std::chrono::seconds( 1 )); // sample with 1Hz
@@ -46,6 +53,7 @@ util::Expected<int32_t> PotManager::humidity() const
             return value; // return error
         }
     }   
+
     // calculate the average 
     float average{ (float) std::accumulate(sensor_data.begin(), 
         sensor_data.end(), 0.0) / sensor_data.size() }; 
@@ -53,15 +61,31 @@ util::Expected<int32_t> PotManager::humidity() const
     print::ok_msg( "Current average humidity: " );
     std::cout << average << '\n';
     #endif
-    return (int32_t) average;
+    return (uint32_t) average;
 }
 
 // getters
+uint32_t PotManager::max() const noexcept {return m_max; }
+uint32_t PotManager::min() const noexcept {return m_min; }
 uint32_t PotManager::sampling_time() const noexcept { return m_sampling_time; }
 SensorType PotManager::sensor() const { return m_sensor_ptr->type(); }
 uint32_t PotManager::threashold() const{ return m_threashold; }
 
 //setters
+void PotManager::set_sensor_minmax(uint32_t min, uint32_t max)
+{
+    if (min <= max){
+        m_min = min;
+        m_max = max;
+    } else {
+        m_min = max;
+        m_max = min; 
+    }
+    print::ok_msg("Allowed sensor range set to: ");
+    std::cout << m_min << " - " << m_max << '\n';
+    m_minmax_is_set = true;
+}
+
 void PotManager::set_sampling_time( uint32_t time )
 {
     if( m_sensor_ptr->type() == SensorType::NO_Sensor){
@@ -78,7 +102,16 @@ void PotManager::set_sampling_time( uint32_t time )
 
 void PotManager::set_treashold( uint32_t value)
 {
-    m_threashold = value;
-    print::ok_msg( "The humidity threashold is set to: ");
-    std::cout << m_threashold << '\n';
+    if (!m_minmax_is_set){
+        print::error_msg("Enter the allowed sensor value range before setting the threashold.\n");
+    } else {
+        if( value <= m_max && value >= m_min) {
+            m_threashold = value;
+            print::ok_msg( "The humidity threashold is set to: ");
+            std::cout << m_threashold << '\n';
+        } else {
+            print::error_msg( "The selected threashold value is not within the range of the allowed sensor values: ");
+            std::cout << m_min << " - " << m_max << '\n'; 
+        }
+    }
 }
--- potManager.hpp
+++ potManager.hpp
@@ -12,18 +12,24 @@ class PotManager
 {
     uint32_t m_sampling_time{ 1 }; // [s]
     std::unique_ptr<SensorMonitor> m_sensor_ptr{};
-    uint32_t m_threashold{ 600 };
+    uint32_t m_threashold{ 0 };
+    uint32_t m_max{ 0 }; // max allowed sensor value  
+    uint32_t m_min{ 0 }; // min allowed sensor value 
+    bool m_minmax_is_set{ false }; // is set true when min and max are set
     uint32_t m_volume{};     // keep track of how much water was added Not implemmente yet!
 
     public:
         PotManager( SensorType );            
-        bool is_dry( util::Expected<int32_t> ) const; 
-        util::Expected<int32_t> humidity() const;
+        bool is_dry( uint32_t ) const; 
+        util::Expected<uint32_t> humidity() const;
 
+        uint32_t max() const noexcept;
+        uint32_t min() const noexcept;
         uint32_t sampling_time() const noexcept;
         SensorType sensor() const;
         uint32_t threashold() const;
         
+        void set_sensor_minmax( uint32_t min, uint32_t max );
         void set_sampling_time( uint32_t );
         void set_treashold( uint32_t );
 };

79f6ec5ec53f74a93c448558a4d022da331e2261
ruthaleks <ruth.aleks@gmail.com>
2019-10-31 17:11:26 +0100

    changed to shorter rutlike type notation

--- utils.hpp
+++ utils.hpp
@@ -7,11 +7,17 @@
 #include "potManager.hpp"
 #include "tankManager.hpp"
 
-using Exp_i32 = const util::Expected<int32_t>;
-using Exp_u32 = const util::Expected<uint32_t>;
+using i8 = int8_t;
+using i16 = int16_t;
 using i32 = int32_t;
+
+using u8  = uint8_t;
+using u16 = uint16_t;
 using u32 = uint32_t;
 
+
+using Exp_i32 = const util::Expected<int32_t>;
+using Exp_u32 = const util::Expected<uint32_t>;
 
 template <typename T> const util::Expected<T> check_type(int32_t value) noexcept;
 Exp_i32 parse_param(uint32_t, const char*) noexcept;

41b37a37aa20103a81bcb093e4aa0ed42e87e760
ruthaleks <ruth.aleks@gmail.com>
2019-11-01 08:12:35 +0100

    restructure

--- unittest/test.cpp
+++ unittest/test.cpp
@@ -6,13 +6,39 @@
 #include "../potManager.hpp"
 #include "../tankManager.hpp"
 
-TEST_CASE( "Test tankManager class", "[tank]" ) {
+TEST_CASE("Tank manager, sensor and actutor types", "[tank]"){
+    
+    SECTION("SIM") {
+        TankManager tank{ SensorType::SIM_LoadSensor, ActuatorType::SIM_Switch};
+        REQUIRE( tank.sensor() == SensorType::SIM_LoadSensor );
+        REQUIRE( tank.actuator() == ActuatorType::SIM_Switch );
+    }
+    SECTION("NO") {
+        TankManager tank{ SensorType::NO_Sensor, ActuatorType::NO_Actuator};
+        REQUIRE( tank.sensor() == SensorType::NO_Sensor );
+        REQUIRE( tank.actuator() == ActuatorType::NO_Actuator );
+    }
+    SECTION("REAL") { // change later to real 
+        TankManager tank{ SensorType::I2C_Sensor, ActuatorType::RELAY_Switch };
+        REQUIRE( tank.sensor() == SensorType::I2C_Sensor );
+        REQUIRE( tank.actuator() == ActuatorType::RELAY_Switch );
+    }
+}
+
+
+TEST_CASE( "Test tank manager methods", "[tank]" ) {
 
     SECTION("Simulated sensor and actuator"){
         TankManager tank{ SensorType::SIM_MoistSensor, ActuatorType::SIM_Switch};
-        REQUIRE( tank.sensor() == SensorType::SIM_MoistSensor );
-        REQUIRE( tank.actuator() == ActuatorType::SIM_Switch );
-        
+        // ----------------------------------------//
+        // check default values 
+        REQUIRE( tank.flow_rate() == 0.0 );
+        REQUIRE( tank.water_amount() == 0 ); 
+        REQUIRE( tank.add_water() == 0);
+        // ----------------------------------------//
+        // special cases
+        tank.set_flow_rate( 10 );
+        REQUIRE( tank.add_water() == 0 ); // water amount is still 0
         // ----------------------------------------//
         // the input is in L/h and the output should be in ml/s
         float c = 1000.0f / 3600.0f;
@@ -21,13 +47,19 @@ TEST_CASE( "Test tankManager class", "[tank]" ) {
 
         tank.set_flow_rate(100);
         REQUIRE( tank.flow_rate() == 100.0f * c );
+
+        tank.set_flow_rate( 0 );
+        REQUIRE( tank.flow_rate() == 0.0 );
+
+        tank.set_flow_rate( -10 );
+        REQUIRE( tank.flow_rate() == -10.0 * c); 
         // ----------------------------------------//
         // the input should be equal to the output
         tank.set_water_amount( 100 );
         REQUIRE( tank.water_amount() == 100 );
 
-        tank.set_water_amount( 1 );
-        REQUIRE( tank.water_amount() == 1);
+        tank.set_water_amount( 0 );
+        REQUIRE( tank.water_amount() == 0);
         // ---------------------------- ------------//
         // ml / (ml/s) = s 
         tank.set_water_amount( 100 );
@@ -37,6 +69,10 @@ TEST_CASE( "Test tankManager class", "[tank]" ) {
         tank.set_water_amount( 13 );
         tank.set_flow_rate( 99 );
         REQUIRE( tank.add_water() == (uint32_t) (13.0f / (99.0f * c) * 1000.0f) );
+        
+        tank.set_water_amount( 13 );
+        tank.set_flow_rate( -80 );
+        REQUIRE( tank.add_water() == (uint32_t) (13.0f / (80.0f * c) * 1000.0f) ); // should return a positive value
         // ---------------------------- ------------//
         // TODO .level()
     }
@@ -70,12 +106,27 @@ TEST_CASE( "Test tankManager class", "[tank]" ) {
     }
 }
 
+TEST_CASE("Pot manager, sensor types", "[pot]"){
+    
+    SECTION("SIM") {
+        PotManager pot{ SensorType::SIM_MoistSensor };
+        REQUIRE( pot.sensor() == SensorType::SIM_MoistSensor );
+    }
+    SECTION("NO") {
+        PotManager pot{ SensorType::NO_Sensor };
+        REQUIRE( pot.sensor() == SensorType::NO_Sensor );
+    }
+    SECTION("REAL") {
+        PotManager pot{ SensorType::I2C_Sensor };
+        REQUIRE( pot.sensor() == SensorType::I2C_Sensor );
+    }
+}
+
 TEST_CASE(" Test pot manager class", "[pot]")
 {
     SECTION("Simulated sensor")
     {
         PotManager pot{ SensorType::SIM_MoistSensor };
-        REQUIRE( pot.sensor() == SensorType::SIM_MoistSensor );
 
         // ---------------------------- ------------//
         // default value check 
